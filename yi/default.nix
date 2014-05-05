1efe5880452b9688a67ee16a8f19d95ddf82fd0d
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-05-05 10:01:05 +0200

    yi

---
+++ yi/default.nix
@@ -0,0 +1,24 @@
+{ haskellPackages ? (import <nixpkgs> {}).haskellPackages_ghc763 }:
+let
+  inherit (haskellPackages) cabal cabalInstall_1_18_0_3 alex
+    binary Cabal cautiousFile concreteTyperep dataDefault derive Diff
+    dlist dyre filepath fingertree glib gtk hashable hint lens mtl
+    pango parsec pointedlist QuickCheck_2_7_3 random regexBase regexTdfa safe
+    split time transformersBase uniplate unixCompat unorderedContainers
+    utf8String vty xdgBasedir tfRandom;
+
+in cabal.mkDerivation (self: {
+  pname = "yi";
+  version = "0.8.1";
+  src = ./src;
+  buildDepends = [
+    # As imported above
+    binary Cabal cautiousFile concreteTyperep dataDefault derive Diff
+    dlist dyre filepath fingertree glib gtk hashable hint lens mtl
+    pango parsec pointedlist QuickCheck_2_7_3 random regexBase regexTdfa safe
+    split time transformersBase uniplate unixCompat unorderedContainers
+    utf8String vty xdgBasedir tfRandom
+  ];
+  buildTools = [ cabalInstall_1_18_0_3 alex ];
+  enableSplitObjs = false;
+})
