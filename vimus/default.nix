415c48019a9a37f49a62adbaf3c7a5ed1767148a
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-05-24 00:08:46 +0200

    Some profiling flags

--- vimus/default.nix
+++ vimus/default.nix
@@ -1,6 +1,12 @@
-{ haskellPackages ? (import <nixpkgs> {}).haskellPackages_ghc763
-, ncursesw ? (import /home/shana/programming/vimus/ncursesw {})
-, libmpd ? (import /home/shana/programming/nix-project-defaults/libmpd-haskell {})
+{ profiling ? false
+, haskellPackages ? (if profiling then (import <nixpkgs> {}).haskellPackages_ghc763_profiling
+                                  else (import <nixpkgs> {}).haskellPackages_ghc763)
+, ncursesw ? (import /home/shana/programming/vimus/ncursesw
+               { haskellPackages = haskellPackages; })
+, libmpd ? (import /home/shana/programming/nix-project-defaults/libmpd-haskell
+             { haskellPackages = haskellPackages;
+               test = false; })
+
 }:
 let
   inherit (haskellPackages) cabal dataDefault deepseq filepath hspec hspecExpectations
@@ -20,6 +26,14 @@ in cabal.mkDerivation (self: {
     ncursesw QuickCheck time transformers wcwidth
   ];
   buildTools = [ cabalInstall ];
+  configureFlags = if profiling then [ "--enable-executable-profiling"
+                                       "--ghc-option=-prof"
+                                       "--ghc-option=-fprof-auto"
+                                       "--ghc-option=-rtsopts"
+                                       "--disable-library-profiling"
+                                     ]
+                                else [];
+  doCheck = ! profiling;
   meta = {
     description = "An MPD client with vim-like key bindings";
     license = self.stdenv.lib.licenses.mit;
