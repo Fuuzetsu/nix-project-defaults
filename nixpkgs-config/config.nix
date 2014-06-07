3490c62a75b7add8bc2a44289968acedf05623a8
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-06-07 22:53:32 +0200

    Haskell package overrides

--- nixpkgs-config/config.nix
+++ nixpkgs-config/config.nix
@@ -10,6 +10,20 @@
     };
   };
 
+  myHaskellPackages_ghc763 = myHaskellPackagesVer pkgs.haskellPackages_ghc763;
+  myHaskellPackages_ghc782 = myHaskellPackagesVer pkgs.haskellPackages_ghc782;
+  myHaskellPackages_ghcHEAD = myHaskellPackagesVer pkgs.haskellPackages_ghcHEAD;
+
+  myHaskellPackagesVer = ver : pkgs.recurseIntoAttrs (ver.override {
+    extension = se : su : {
+      vty_5_1_0 = se.callPackage /home/shana/programming/nix-project-defaults/vty/5.1.0.nix {
+        Cabal = se.Cabal_1_20_0_0;
+      };
+      testFrameworkSmallcheck =
+        se.callPackage /home/shana/programming/nix-project-defaults/test-framework-smallcheck {};
+    };
+  });
+
   cantataNixpkgs = self.cantata;
   cantata = lib.overrideDerivation # Local SVN checkout
                (cantataNixpkgs.override { withQt4 = false; withQt5 = true; })
