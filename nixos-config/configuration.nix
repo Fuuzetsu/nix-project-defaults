682134214842131f17a60762488d24b10e635f17
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-06-11 19:18:56 +0200

    RIP .15

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -111,11 +111,11 @@
   # NFS
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /export                 192.168.1.10(rw,fsid=0,no_subtree_check) 192.168.1.15(rw,fsid=0,no_subtree_check)
-    /export/kotomi          192.168.1.10(rw,nohide,insecure,no_subtree_check) 192.168.1.15(rw,nohide,insecure,no_subtree_check)
-    /export/mafuyu          192.168.1.10(rw,nohide,insecure,no_subtree_check) 192.168.1.15(rw,nohide,insecure,no_subtree_check)
-    /export/sen             192.168.1.10(rw,nohide,insecure,no_subtree_check) 192.168.1.15(rw,nohide,insecure,no_subtree_check)
-    /export/tomoyo          192.168.1.10(rw,nohide,insecure,no_subtree_check) 192.168.1.15(rw,nohide,insecure,no_subtree_check)
+    /export                 192.168.1.10(rw,fsid=0,no_subtree_check)
+    /export/kotomi          192.168.1.10(rw,nohide,insecure,no_subtree_check)
+    /export/mafuyu          192.168.1.10(rw,nohide,insecure,no_subtree_check)
+    /export/sen             192.168.1.10(rw,nohide,insecure,no_subtree_check)
+    /export/tomoyo          192.168.1.10(rw,nohide,insecure,no_subtree_check)
   '';
 
   services.nfs.server.createMountPoints = true;
