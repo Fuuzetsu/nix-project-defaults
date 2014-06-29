fe3a6e1cb581c1dd58e3e7b78039d5905ff41d7b
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-06-29 17:29:54 +0200

    Hook up profiling/no_profiling libs for ghcHEAD

--- nixpkgs-config/config.nix
+++ nixpkgs-config/config.nix
@@ -1,18 +1,8 @@
 { pkgs }:
 
-{ packageOverrides = self: with pkgs; rec {
-
-  haskellPackages_ghcHEAD = self.haskell.packages {
-    ghcPath = /home/shana/programming/ghc;
-    ghcBinary = self.haskellPackages.ghcPlain;
-    prefFun = self.haskell.ghcHEADPrefs;
-  };
-
-  myHaskellPackages_ghc763 = myHaskellPackagesVer pkgs.haskellPackages_ghc763;
-  myHaskellPackages_ghc782 = myHaskellPackagesVer pkgs.haskellPackages_ghc782;
-
-  myHaskellPackages_ghcHEAD = pkgs.recurseIntoAttrs (haskellPackages_ghcHEAD.override {
-    extension = se : su : {
+with pkgs;
+let ghcHEAD_overrides = ps: recurseIntoAttrs (ps.override {
+      extension = se : su : {
       syb = se.callPackage /home/shana/programming/nixpkgs/pkgs/development/libraries/haskell/syb/0.4.2.nix {};
       vty_5_1_0 = se.callPackage /home/shana/programming/nix-project-defaults/vty/5.1.0.nix {};
       mtl = se.callPackage /home/shana/programming/nix-project-defaults/mtl/2.2.1.nix {};
@@ -21,7 +11,23 @@
     };
   });
 
-  myHaskellPackagesVer = ver : pkgs.recurseIntoAttrs (ver.override {
+in
+{ packageOverrides = self: rec {
+
+  packages_ghcHEAD = self.haskell.packages {
+    ghcPath = /home/shana/programming/ghc;
+    ghcBinary = self.haskellPackages.ghcPlain;
+    prefFun = self.haskell.ghcHEADPrefs;
+  };
+
+  haskellPackages_ghcHEAD_no_profiling = ghcHEAD_overrides (recurseIntoAttrs packages_ghcHEAD.noProfiling);
+  haskellPackages_ghcHEAD_profiling = ghcHEAD_overrides (recurseIntoAttrs packages_ghcHEAD.profiling);
+  haskellPackages_ghcHEAD = ghcHEAD_overrides (recurseIntoAttrs packages_ghcHEAD.highPrio);
+
+  myHaskellPackages_ghc763 = myHaskellPackagesVer haskellPackages_ghc763;
+  myHaskellPackages_ghc782 = myHaskellPackagesVer haskellPackages_ghc782;
+
+  myHaskellPackagesVer = ver : recurseIntoAttrs (ver.override {
     extension = se : su : {
       vty_5_1_0 = se.callPackage /home/shana/programming/nix-project-defaults/vty/5.1.0.nix {
         Cabal = se.Cabal_1_20_0_0;
