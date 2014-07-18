f45ac1f5a2fde2d0f74cddb71ed3238a5ded83a2
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-07-18 02:42:47 +0200

    extra packages for doc building

--- haddock/default.nix
+++ haddock/default.nix
@@ -1,4 +1,9 @@
-{ haskellPackages ? (import <nixpkgs> {}).myHaskellPackages_ghcHEAD
+{ haskellPackages ? (import <nixpkgs> {}).haskellPackages_ghc783
+, pkgs ? (import <nixpkgs> {})
+, autoconf ? pkgs.autoconf
+, libxslt ? pkgs.libxslt
+, libxml2 ? pkgs.libxml2
+, texLive ? pkgs.texLive
 , haddockLibrary ? (import /home/shana/programming/haddock/haddock-library
     { haskellPackages = haskellPackages; })
 }:
@@ -8,11 +13,17 @@ haskellPackages.cabal.mkDerivation (self: {
   version = "2.15.0";
   src = /home/shana/programming/haddock;
   buildDepends = with haskellPackages;
-                   [ Cabal deepseq filepath ghcPaths xhtml haddockLibrary ];
+                   [ Cabal deepseq filepath ghcPaths xhtml haddockLibrary
+                     autoconf libxslt libxml2 texLive
+                   ];
   testDepends = with haskellPackages; [ Cabal deepseq filepath hspec QuickCheck ];
   isLibrary = true;
   isExecutable = true;
   enableSplitObjs = false;
-  noHaddock = true;
+  noHaddock = false;
   doCheck = true;
+  # extraConfigureFlags = [ "--ghc-option=-fprof-auto"
+  #                         "--ghc-option=-rtsopts"
+  #                         "--enable-executable-profiling"
+  #                       ];
 })
