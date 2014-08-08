29d705ff9f2d764c2edebac0d46db937bdcc4205
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-08-08 23:21:15 +0200

    dlopen patches for wlc

--- wlc/default.nix
+++ wlc/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchgit, cmake, pkgconfig, wayland
-, pixman, libxkbcommon, mesa, libdrm, libX11, udev
+{ stdenv, fetchgit, cmake, pkgconfig, wayland, pixman, libxkbcommon
+, mesa, libdrm, libX11, udev, libxcb
 }:
 
 stdenv.mkDerivation rec {
@@ -15,8 +15,16 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ cmake pkgconfig wayland pixman
-                  libxkbcommon mesa libdrm libX11 udev
+                  libxkbcommon mesa libdrm libX11 udev libxcb
                 ];
+
+  preConfigure = ''
+    sed -i "s@libX11.so@${libX11}/lib/libX11.so@g" src/backend/x11.c
+    sed -i "s@libX11-xcb.so@${libX11}/lib/libX11-xcb.so@g" src/backend/x11.c
+    sed -i "s@libxcb.so@${libxcb}/lib/libxcb.so@g" src/backend/x11.c
+    #sed -i "s@libEGL.so@${mesa}/lib/libEGL.so@g" src/context/egl.c
+    #sed -i "s@libGLESv2.so@${mesa}/lib/libGLESv2.so@g" src/render/gles2.c
+  '';
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/Cloudef/wlc";
