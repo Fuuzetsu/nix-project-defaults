65669790a2bf9321ce18c242f48a91734f7216d3
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-08-08 21:38:05 +0200

    rtorrent-state

---
+++ rtorrent-state/default.nix
@@ -0,0 +1,16 @@
+{ haskellPackages ? (import <nixpkgs> {}).myHaskellPackages_ghc783
+}:
+
+haskellPackages.cabal.mkDerivation (self: {
+  pname = "rtorrent-state";
+  version = "0.1.0.0";
+  src = /home/shana/programming/rtorrent-state;
+  buildDepends = with haskellPackages; [ bencoding utf8String lens ];
+  testDepends = with haskellPackages; [ QuickCheck hspec bencoding utf8String temporary ];
+  meta = {
+    homepage = "http://github.com/Fuuzetsu/rtorrent-state";
+    description = "Parsing and manipulation of rtorrent state file contents";
+    license = self.stdenv.lib.licenses.gpl3;
+    platforms = self.ghc.meta.platforms;
+  };
+})
