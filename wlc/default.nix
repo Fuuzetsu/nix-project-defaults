ad3209ed48202f80efa725833a204465fc51c61f
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-08-09 02:27:52 +0200

    more wlc munging

--- wlc/default.nix
+++ wlc/default.nix
@@ -1,29 +1,29 @@
 { stdenv, fetchgit, cmake, pkgconfig, wayland, pixman, libxkbcommon
-, mesa, libdrm, libX11, udev, libxcb
+, libdrm, libX11, udev, libxcb, mesa, libXext
 }:
 
 stdenv.mkDerivation rec {
   pname = "wlc";
-  version = "710819e3fccdce15d0f41791a975c672e3c1b2bc";
+  version = "2a95f076d9da424a5496a0b303c21f0be478d3d1";
   name = pname + "-" + version;
 
   src = fetchgit {
     url = "https://github.com/Cloudef/wlc.git";
-    sha256 = "145pnqcrmbfkzg5yvr7q2rynl40gpy8b49nc1s5p5qng02wyy34q";
+    sha256 = "0kd7l52ilk4d8vq0g0is68mvjg67ag8mgrpj3wwx4vx775xfzp7x";
     rev = version;
     fetchSubmodules = false;
   };
 
-  buildInputs = [ cmake pkgconfig wayland pixman
-                  libxkbcommon mesa libdrm libX11 udev libxcb
+  buildInputs = [ cmake pkgconfig wayland pixman mesa libXext
+                  libxkbcommon libdrm libX11 udev libxcb
                 ];
 
   preConfigure = ''
     sed -i "s@libX11.so@${libX11}/lib/libX11.so@g" src/backend/x11.c
     sed -i "s@libX11-xcb.so@${libX11}/lib/libX11-xcb.so@g" src/backend/x11.c
     sed -i "s@libxcb.so@${libxcb}/lib/libxcb.so@g" src/backend/x11.c
-    #sed -i "s@libEGL.so@${mesa}/lib/libEGL.so@g" src/context/egl.c
-    #sed -i "s@libGLESv2.so@${mesa}/lib/libGLESv2.so@g" src/render/gles2.c
+    sed -i "s@libEGL.so@/run/opengl-driver/lib/libEGL.so@g" src/context/egl.c
+    sed -i "s@libGLESv2.so@/run/opengl-driver/lib/libGLESv2.so@g" src/render/gles2.c
   '';
 
   meta = with stdenv.lib; {
