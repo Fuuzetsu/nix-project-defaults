4cfbe845998d937b2479c94a8b78cada9bdcb20c
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-10-07 18:55:53 +0100

    auto-detect cabal2nix version

--- cabal2nix/default.nix
+++ cabal2nix/default.nix
@@ -4,10 +4,19 @@
 , transformers
 }:
 
+let pkgs = import <nixpkgs> {};
+    lib = pkgs.lib;
+    sr  = "/home/shana/programming/cabal2nix";
+    file = builtins.readFile (sr + "/cabal2nix.cabal");
+    strs = lib.strings.splitString "\n" file;
+    vstr = builtins.head (builtins.filter (s: lib.strings.hasPrefix "Version:" s) strs);
+    vrsn = lib.strings.removePrefix "Version:" (lib.strings.replaceChars [" "] [""] vstr);
+
+in
 cabal.mkDerivation (self: {
   pname = "cabal2nix";
-  version = "1.69";
-  src = /home/shana/programming/cabal2nix;
+  version = vrsn;
+  src = sr;
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
