20ab86ec5f1488ae6aebfca220fb9d97d2bf065c
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2015-03-09 01:14:21 +0000

    yi-rope to haskell-ng

--- yi-rope/shell.nix
+++ yi-rope/shell.nix
@@ -1,11 +1,3 @@
 let pkgs = import <nixpkgs> {};
-    myHaskellPackages = pkgs.myHaskellPackages;
-    haskellPackages = myHaskellPackages.override {
-      extension = self: super: {
-        yiRope = myHaskellPackages.callPackage ./. {};
-      };
-    };
-in pkgs.lib.overrideDerivation haskellPackages.yiRope (attrs: {
-  noHaddock = true;
-  buildInputs = [ ] ++ attrs.buildInputs;
-})
+    packageSet = pkgs.haskell-ng.packages.ghc784;
+in packageSet.callPackage ./. {}
