6c6848596b61e490479dc5ca252de70e24f47535
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2015-04-18 21:10:28 +0100

    PastePipe: to ng

--- PastePipe/shell.nix
+++ PastePipe/shell.nix
@@ -1,10 +1,3 @@
 let pkgs = import <nixpkgs> {};
-    myHaskellPackages = pkgs.myHaskellPackages_nixpkgs;
-    haskellPackages = myHaskellPackages.override {
-      extension = self: super: {
-        PastePipe = myHaskellPackages.callPackage ./. {};
-      };
-    };
-in pkgs.lib.overrideDerivation haskellPackages.PastePipe (attrs: {
-  buildInputs = [ ] ++ attrs.buildInputs;
-})
+    packageSet = pkgs.haskell-ng.packages.ghc7101;
+in packageSet.callPackage ./. {}
