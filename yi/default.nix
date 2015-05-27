ff9749fc6705416d8691384c25fa65269cab40c5
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2015-05-27 04:08:22 +0100

    Update Yi exprs

--- yi/default.nix
+++ yi/default.nix
@@ -1,34 +1,32 @@
-{ mkDerivation, array, base, binary, bytestring, Cabal
-, containers, data-default, directory, dlist
-, dynamic-state, dyre, exceptions, filepath, glib, gtk, hashable
-, HUnit, lens, mtl, old-locale, oo-prototypes, pango, parsec
-, pointedlist, process, QuickCheck, random, regex-base, regex-tdfa
-, safe, semigroups, split, stdenv, tasty, tasty-hunit
-, tasty-quickcheck, template-haskell, text, time, transformers-base
-, unix, unix-compat, unordered-containers, vty, word-trie
-, xdg-basedir, yi-language, yi-rope, cabal-install
+{ mkDerivation, array, base, binary, bytestring, Cabal, containers
+, data-default, directory, dlist, dynamic-state, dyre, exceptions
+, filepath, glib, gtk, hashable, HUnit, lens, mtl, old-locale
+, oo-prototypes, pango, parsec, pointedlist, process, QuickCheck
+, random, regex-base, regex-tdfa, safe, semigroups, split, stdenv
+, tasty, tasty-hunit, tasty-quickcheck, template-haskell, text
+, time, transformers-base, unix, unix-compat, unordered-containers
+, vty, word-trie, xdg-basedir, yi-language, yi-rope
 }:
 mkDerivation {
   pname = "yi";
-  version = "0.11.2";
+  version = "0.12.0";
   src = /home/shana/programming/yi;
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    array base binary bytestring Cabal containers
-    data-default directory dlist dynamic-state dyre exceptions filepath
-    glib gtk hashable lens mtl old-locale oo-prototypes pango parsec
-    pointedlist process QuickCheck random regex-base regex-tdfa safe
-    semigroups split template-haskell text time transformers-base unix
-    unix-compat unordered-containers vty word-trie xdg-basedir
-    yi-language yi-rope cabal-install
+    array base binary bytestring Cabal containers data-default
+    directory dlist dynamic-state dyre exceptions filepath glib gtk
+    hashable lens mtl old-locale oo-prototypes pango parsec pointedlist
+    process QuickCheck random regex-base regex-tdfa safe semigroups
+    split template-haskell text time transformers-base unix unix-compat
+    unordered-containers vty word-trie xdg-basedir yi-language yi-rope
   ];
   testDepends = [
     base directory filepath HUnit lens QuickCheck semigroups tasty
     tasty-hunit tasty-quickcheck text yi-language yi-rope
   ];
   configureFlags = [ "-fpango" "-fvty" ];
-  homepage = "http://haskell.org/haskellwiki/Yi";
+  homepage = "https://yi-editor.github.io";
   description = "The Haskell-Scriptable Editor";
   license = stdenv.lib.licenses.gpl2;
 }
